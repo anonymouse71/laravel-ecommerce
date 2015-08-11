f206d392359834f041b90c7b0fe96f18f5567794
Gautam Fonseca <gautamz07@gmail.com>
2015-08-11 19:45:42 +0530

    initial commit

---
+++ app/controllers/CategoriesController.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ *
+ */
+class categoriesController extends BaseController {
+
+  public function __construct() {
+    $this->beforeFilter('csrf' , array('on'=>'post'));
+  }
+
+  public function getIndex() {
+      return View::make('categories.index')
+        ->with('categories' , Category::all());
+  }
+
+  public function postCreate() {
+
+    $validator = Validator::make(Input::all() , Category::$rules);
+
+    if($validator->passes()) {
+      $category = new Category;
+      $category->name = Input::get('name');
+      $category->save();
+
+      return Redirect::to('admin/categories/index')
+         ->with('message' , 'Category created');
+    }
+
+    return Redirect::to('admin/categories/index')
+      ->with('message' , 'something went wrong')
+      ->withError($validator)
+      ->withInput();
+
+  }
+
+  public function postDestroy() {
+
+      $category = Category::find(Input::get('id'));
+      if ($category) {
+        $category->delete();
+        return Redirect::to('admin/categories/index')
+        ->with('message' , 'category has been deleted');
+      }
+
+      return Redirect::to('admin/categories/index')
+        ->with('message' , 'something went wrong , Please try again');
+
+  }
+
+}
+
+
+
+?>
